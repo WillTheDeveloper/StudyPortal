5214c1ded622da3c8d1fbc1bd647666770a495e3
Will <will@teamatlas.cloud>
2021-10-03 17:56:47 +0100

    Installed telescope and breeze. Deleted redundant pages.

--- routes/web.php
+++ routes/web.php
@@ -13,30 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Just a beta home page
 Route::get('/', function () {
     return view('home');
 });
 
-// Concept pages
-Route::get('/concept/dashboard', function () {
-    return view('concept.dashboard');
-});
-Route::get('/concept/assignments', function () {
-    return view('concept.assignments');
-});
-Route::get('/concept/resources', function () {
-    return view('concept.resources');
-});
-Route::get('/concept/community', function () {
-    return view('concept.community');
-});
-Route::get('/concept/timetable', function () {
-    return view('concept.timetable');
-});
-Route::get('/concept/settings', function () {
-    return view('concept.settings');
-});
-Route::get('/concept/resources/webdevelopment', function () {
-    return view('concept.resources.webdevelopment');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [
-      './resources/**/*.blade.php',
-      './resources/**/*.js',
-      './resources/**/*.vue',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-        colors: {
-            rose: colors.rose,
-            sky: colors.sky,
-            teal: colors.teal,
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                rose: colors.rose,
+            },
         },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-      require('@tailwindcss/forms'),
-  ],
-}
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
