4e4f21884768bf7206fb1c0a485f5c969c2b538a
Will <will@teamatlas.cloud>
2021-10-03 19:51:13 +0100

    Edited the subjects filter on community page

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -83,34 +83,34 @@
                     <div class="pt-10">
                         <p class="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider"
                            id="communities-headline">
-                            My communities
+                            My subjects
                         </p>
                         <div class="mt-3 space-y-2" aria-labelledby="communities-headline">
                             <a href="#"
                                class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
                 <span class="truncate">
-                  Movies
+                  Computer Science
                 </span>
                             </a>
 
                             <a href="#"
                                class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
                 <span class="truncate">
-                  Food
+                  Hair and Beauty
                 </span>
                             </a>
 
                             <a href="#"
                                class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
                 <span class="truncate">
-                  Sports
+                  Psychology
                 </span>
                             </a>
 
                             <a href="#"
                                class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
                 <span class="truncate">
-                  Animals
+                  Science
                 </span>
                             </a>
 
