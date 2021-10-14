92ed43da5c357443dc2a88f0804864f3e6ecebe9
Will <will@teamatlas.cloud>
2021-10-14 17:18:14 +0100

    Updated newsletter area on pricing page

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -1551,7 +1551,7 @@
                         Sign up for our newsletter
                     </h2>
                     <p class="mt-3 max-w-3xl text-lg text-gray-500">
-                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.
+                        Our newsletters have information regarding updates and other important information.
                     </p>
                 </div>
                 <div class="mt-8 lg:mt-0 lg:ml-8">
@@ -1560,7 +1560,7 @@
                         <input id="email-address" name="email-address" type="email" autocomplete="email" required class="w-full px-5 py-3 border border-gray-300 shadow-sm placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs rounded-md" placeholder="Enter your email">
                         <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
                             <button type="submit" class="w-full flex items-center justify-center py-3 px-5 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                Notify me
+                                Sign up
                             </button>
                         </div>
                     </form>
