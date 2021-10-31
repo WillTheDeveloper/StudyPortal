eacf9aecb37eaef6e106a85de0257284b4a7aba5
Will <twitchifygamers@gmail.com>
2021-10-31 09:34:54 +0000

    Minor group change. Removed edit button on mobile.

--- resources/views/managegroups.blade.php
+++ resources/views/managegroups.blade.php
@@ -46,9 +46,9 @@
                                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 Role
                                             </th>
-                                            <th scope="col" class="relative px-6 py-3">
+                                            {{--<th scope="col" class="relative px-6 py-3">
                                                 <span class="sr-only">Edit</span>
-                                            </th>
+                                            </th>--}}
                                         </tr>
                                         </thead>
                                         <tbody class="bg-white divide-y divide-gray-200">
