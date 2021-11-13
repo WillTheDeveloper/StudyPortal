75624a63ffd419366396d427c2027f20dca69727
Will <twitchifygamers@gmail.com>
2021-11-13 21:40:08 +0000

    Redesigned the community user's pages

--- resources/views/communityuser.blade.php
+++ resources/views/communityuser.blade.php
@@ -6,91 +6,160 @@
     </x-slot>
 
     <!-- This example requires Tailwind CSS v2.0+ -->
+    <div>
+        <div>
+            <img class="h-32 w-full object-cover lg:h-48" src="https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="">
+        </div>
+        <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+                <div class="flex">
+                    <img class="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
+                </div>
+                <div class="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                    <div class="sm:hidden md:block mt-6 min-w-0 flex-1">
+                        <h1 class="text-2xl font-bold text-gray-900 truncate">
+                            {{$user->name}}
+                        </h1>
+                    </div>
+                    <div class="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
+                        <button type="button" class="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500">
+                            <!-- Heroicon name: solid/mail -->
+                            <svg class="-ml-1 mr-2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                            </svg>
+                            <span>Message</span>
+                        </button>
+                        <button type="button" class="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500">
+                            <!-- Heroicon name: solid/phone -->
+                            <svg class="-ml-1 mr-2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                            </svg>
+                            <span>Call</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="hidden sm:block md:hidden mt-6 min-w-0 flex-1">
+                <h1 class="text-2xl font-bold text-gray-900 truncate">
+                    {{$user->name}}
+                </h1>
+            </div>
+
+            <dl class="mt-3 mb-3 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+                    <dt class="text-sm font-medium text-gray-500 truncate">
+                        Posts
+                    </dt>
+                    <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                        {{$user->Post->count()}}
+                    </dd>
+                </div>
+
+                <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+                    <dt class="text-sm font-medium text-gray-500 truncate">
+                        Comments
+                    </dt>
+                    <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                        {{$user->Comment->count()}}
+                    </dd>
+                </div>
+
+                <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+                    <dt class="text-sm font-medium text-gray-500 truncate">
+                        Likes
+                    </dt>
+                    <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                        100%
+                    </dd>
+                </div>
+            </dl>
+
+        </div>
+    </div>
+
+    <!-- This example requires Tailwind CSS v2.0+ -->
     <div class="bg-white">
         <div class="max-w-2xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
+
+            <!-- This example requires Tailwind CSS v2.0+ -->
             <div>
-                <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">{{$user->name}}</h2>
-                @isset($user->bio)
-                    <p class="mt-4 text-gray-500">{{$user->bio}}</p>
-                @endisset
 
-                <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-                    @isset($user->username)
-                        <div class="border-t border-gray-200 pt-4">
-                            <dt class="font-medium text-gray-900">Username</dt>
-                            <dd class="mt-2 text-sm text-gray-500">{{$user->username}}</dd>
-                        </div>
-                    @endisset
 
-                    <div class="border-t border-gray-200 pt-4">
-                        <dt class="font-medium text-gray-900">Subjects</dt>
-                        <dd class="mt-2 text-sm text-gray-500">
-                            @foreach($user->Subject as $subject)
-                                {{$subject->subject}}<br>
-                            @endforeach
-                        </dd>
-                    </div>
+                <div class="flow-root">
+                    <ul role="list" class="-mb-8">
+                        <li>
+                            <div class="relative pb-8">
+                                <span class="absolute top-5 left-5 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
+                                <div class="relative flex items-start space-x-3">
+                                    <div class="relative">
+                                        <img class="h-10 w-10 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white" src="https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80" alt="">
 
-                    <div class="border-t border-gray-200 pt-4">
-                        <dt class="font-medium text-gray-900">Posts</dt>
-                        <dd class="mt-2 text-sm text-gray-500">{{$user->Post->count()}}</dd>
-                    </div>
-
-                    <div class="border-t border-gray-200 pt-4">
-                        <dt class="font-medium text-gray-900">Comments</dt>
-                        <dd class="mt-2 text-sm text-gray-500">{{$user->Comment->count()}}</dd>
-                    </div>
-
-                    <div class="border-t border-gray-200 pt-4">
-                        <dt class="font-medium text-gray-900">Includes</dt>
-                        <dd class="mt-2 text-sm text-gray-500">Wood card tray and 3 refill packs</dd>
-                    </div>
-
-                    <div class="border-t border-gray-200 pt-4">
-                        <dt class="font-medium text-gray-900">Considerations</dt>
-                        <dd class="mt-2 text-sm text-gray-500">Made from natural materials. Grain and color vary with each item.</dd>
-                    </div>
-                </dl>
-            </div>
-            <div class="grid grid-cols-2 gap-4 sm:gap-6 lg:gap-8">
-                <div>
-                    <p>{{$user->name}}'s Posts.</p> <br>
-                    <ul role="list" class="divide-y divide-gray-200">
-                        @foreach($user->Post as $post)
-                        <li class="py-4">
-                            <div class="flex space-x-3">
-                                <div class="flex-1 space-y-1">
-                                    <div class="flex items-center justify-between">
-                                        <h3 class="text-sm font-medium">{{$user->name}}</h3>
-                                        <p class="text-sm text-gray-500">{{$post->created_at->diffForHumans()}}</p>
+                                        <span class="absolute -bottom-0.5 -right-1 bg-white rounded-tl px-0.5 py-px">
+              <!-- Heroicon name: solid/chat-alt -->
+              <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd" />
+              </svg>
+            </span>
                                     </div>
-                                    <p class="text-sm text-gray-500">Posted {{$post->title}} in {{$post->Subject->subject}}</p>
+                                    <div class="min-w-0 flex-1">
+                                        <div>
+                                            <div class="text-sm">
+                                                <a href="#" class="font-medium text-gray-900">Eduardo Benz</a>
+                                            </div>
+                                            <p class="mt-0.5 text-sm text-gray-500">
+                                                Commented 6d ago
+                                            </p>
+                                        </div>
+                                        <div class="mt-2 text-sm text-gray-700">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam.
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </li>
-                        @endforeach
+
+                        <li>
+                            <div class="relative pb-8">
+                                <div class="relative flex items-start space-x-3">
+                                    <div class="relative">
+                                        <img class="h-10 w-10 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white" src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80" alt="">
+
+                                        <span class="absolute -bottom-0.5 -right-1 bg-white rounded-tl px-0.5 py-px">
+              <!-- Heroicon name: solid/chat-alt -->
+              <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd" />
+              </svg>
+            </span>
+                                    </div>
+                                    <div class="min-w-0 flex-1">
+                                        <div>
+                                            <div class="text-sm">
+                                                <a href="#" class="font-medium text-gray-900">Jason Meyers</a>
+                                            </div>
+                                            <p class="mt-0.5 text-sm text-gray-500">
+                                                Commented 2h ago
+                                            </p>
+                                        </div>
+                                        <div class="mt-2 text-sm text-gray-700">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. Scelerisque amet elit non sit ut tincidunt condimentum. Nisl ultrices eu venenatis diam.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
                     </ul>
                 </div>
 
-                <div>
-                    <p>{{$user->name}}'s Comments.</p> <br>
-                    <ul role="list" class="divide-y divide-gray-200">
-                        @foreach($user->Comment as $comment)
-                            <li class="py-4">
-                                <div class="flex space-x-3">
-                                    <div class="flex-1 space-y-1">
-                                        <div class="flex items-center justify-between">
-                                            <h3 class="text-sm font-medium">{{$user->name}}</h3>
-                                            <p class="text-sm text-gray-500">{{$comment->created_at->diffForHumans()}}</p>
-                                        </div>
-                                        <p class="text-sm text-gray-500">Said "{{$comment->comment}}" on {{$comment->Post->title}}</p>
-                                    </div>
-                                </div>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
             </div>
+
+
+
+
         </div>
     </div>
 
