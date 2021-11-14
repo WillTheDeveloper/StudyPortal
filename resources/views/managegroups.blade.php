3700ac532d37127cd79352ddeab3b739f52ac6d1
Will <twitchifygamers@gmail.com>
2021-11-14 09:08:44 +0000

    Created empty state for managing groups

--- resources/views/managegroups.blade.php
+++ resources/views/managegroups.blade.php
@@ -58,7 +58,7 @@
                                         </tr>
                                         </thead>
                                         <tbody class="bg-white divide-y divide-gray-200">
-                                        @foreach($groupstuff->User->sortBy('name') as $users)
+                                        @forelse($groupstuff->User->sortBy('name') as $users)
                                         <tr>
                                             <td class="px-6 py-4 whitespace-nowrap">
                                                 <div class="flex items-center">
@@ -91,7 +91,17 @@
                                                 <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                                             </td>
                                         </tr>
-                                        @endforeach
+                                        @empty
+                                                <div class="text-center pt-4 pb-4">
+                                                    <svg class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                                        <path vector-effect="non-scaling-stroke" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
+                                                    </svg>
+                                                    <h3 class="mt-2 text-sm font-medium text-gray-900">No students</h3>
+                                                    <p class="mt-1 text-sm text-gray-500">
+                                                        There are no students in this group, add them in to see them here.
+                                                    </p>
+                                                </div>
+                                        @endforelse
 
 
 
