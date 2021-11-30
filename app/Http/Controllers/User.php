403b2c3835bd43c8deab6214df6d1fd2aea726d9
Will <twitchifygamers@gmail.com>
2021-11-30 21:01:33 +0000

    Removed unused pagination import from user controller

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -3,12 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Livewire\WithPagination;
 
 class User extends Controller
 {
-    use WithPagination;
-
     public function showAll()
     {
         if (auth()->user()->is_admin) {
