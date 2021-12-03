3f90ce3e17a4c71c3001b49624d32bcf8da7002a
Will <twitchifygamers@gmail.com>
2021-12-03 16:20:56 +0000

    Made teams group listing dynamic

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -83,26 +83,14 @@
                         Teams
                     </h3>
                     <div class="mt-1 space-y-1" role="group" aria-labelledby="mobile-teams-headline">
+                        @foreach(auth()->user()->Group()->get() as $group)
                         <a href="#" class="group flex items-center px-3 py-2 text-base leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
                             <span class="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full" aria-hidden="true"></span>
                             <span class="truncate">
-                  Engineering
-                </span>
+                              {{$group->name}}
+                            </span>
                         </a>
-
-                        <a href="#" class="group flex items-center px-3 py-2 text-base leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
-                            <span class="w-2.5 h-2.5 mr-4 bg-green-500 rounded-full" aria-hidden="true"></span>
-                            <span class="truncate">
-                  Human Resources
-                </span>
-                        </a>
-
-                        <a href="#" class="group flex items-center px-3 py-2 text-base leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
-                            <span class="w-2.5 h-2.5 mr-4 bg-yellow-500 rounded-full" aria-hidden="true"></span>
-                            <span class="truncate">
-                  Customer Success
-                </span>
-                        </a>
+                        @endforeach
                     </div>
                 </div>
             </nav>
@@ -218,26 +206,14 @@
                     Teams
                 </h3>
                 <div class="mt-1 space-y-1" role="group" aria-labelledby="desktop-teams-headline">
+                    @foreach(auth()->user()->Group()->get() as $group)
                     <a href="#" class="group flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50">
                         <span class="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full" aria-hidden="true"></span>
                         <span class="truncate">
-                Engineering
-              </span>
+                            {{$group->name}}
+                          </span>
                     </a>
-
-                    <a href="#" class="group flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50">
-                        <span class="w-2.5 h-2.5 mr-4 bg-green-500 rounded-full" aria-hidden="true"></span>
-                        <span class="truncate">
-                Human Resources
-              </span>
-                    </a>
-
-                    <a href="#" class="group flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50">
-                        <span class="w-2.5 h-2.5 mr-4 bg-yellow-500 rounded-full" aria-hidden="true"></span>
-                        <span class="truncate">
-                Customer Success
-              </span>
-                    </a>
+                    @endforeach
                 </div>
             </div>
         </nav>
