05f81768e865a150b303331beb0859949989f0ca
Will <twitchifygamers@gmail.com>
2021-12-03 17:37:03 +0000

    Updated header text of the page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -99,7 +99,7 @@
                 <div class="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
                     <div class="flex-1 min-w-0">
                         <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">
-                            Home
+                            Profile
                         </h1>
                     </div>
                     <div class="mt-4 flex sm:mt-0 sm:ml-4">
