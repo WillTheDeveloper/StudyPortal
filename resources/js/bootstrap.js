609cd87be2b82444a510cff3d679c48bb7052aab
Will <twitchifygamers@gmail.com>
2021-12-13 19:56:45 +0000

    Random typo in file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,7 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
- * Echo exposes an expressive API for subscribing to channels and listening
+ * Echo exposes an expressive API for subscribing to channel and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
