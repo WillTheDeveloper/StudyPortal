c93c78250e5d416930c42f14962bd262dc4ed378
Will <twitchifygamers@gmail.com>
2021-12-21 10:56:27 +0000

    Updated button text

--- resources/views/communities.blade.php
+++ resources/views/communities.blade.php
@@ -212,7 +212,7 @@
                                                      aria-hidden="true">
                                                   <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"/>
                                                 </svg>
-                                                <span class="font-medium text-gray-900">Share</span>
+                                                <span class="font-medium text-gray-900">Join</span>
                                               </button>
                                             </span>
                                         </div>
