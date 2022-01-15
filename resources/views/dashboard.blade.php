96e87730ab4ae864aadf0e70a2e57a096db63173
Will <twitchifygamers@gmail.com>
2022-01-15 16:38:59 +0000

    Commented the share and create button on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -102,7 +102,7 @@
                             Dashboard
                         </h1>
                     </div>
-                    <div class="mt-4 flex sm:mt-0 sm:ml-4">
+                    {{--<div class="mt-4 flex sm:mt-0 sm:ml-4">
                         <button type="button"
                                 class="order-1 ml-3 inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-0 sm:ml-0">
                             Share
@@ -111,7 +111,7 @@
                                 class="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3">
                             Create
                         </button>
-                    </div>
+                    </div>--}}
                 </div>
                 <!-- Pinned projects -->
                 <div class="px-4 mt-6 sm:px-6 lg:px-8">
