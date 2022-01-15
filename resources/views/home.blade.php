7e385fd034daa553b1271d9ccc50634707bddb2c
Will <twitchifygamers@gmail.com>
2022-01-15 17:26:14 +0000

    Fixed typo route name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -52,7 +52,7 @@
                     <a href="{{route('features')}}" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                         Features
                     </a>
-                    <a href="{{route('features')}}" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                    <a href="{{route('pricing')}}" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                         Pricing
                     </a>
                     <a href="{{route('contact')}}" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
