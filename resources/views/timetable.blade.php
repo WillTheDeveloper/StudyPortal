6c41a39de549cd44683736b5b4b96f2e8966415f
Will <twitchifygamers@gmail.com>
2022-02-08 11:52:42 +0000

    Started working out spacing for timetable

--- resources/views/timetable.blade.php
+++ resources/views/timetable.blade.php
@@ -224,17 +224,23 @@
                                                 </div>
 
                                                 <!-- Events -->
-                                                <ol class="col-start-1 col-end-2 row-start-1 grid grid-cols-1 sm:grid-cols-7 sm:pr-8" style="grid-template-rows: 1.75rem repeat(288, minmax(0, 1fr)) auto">
-                                                    <li class="relative mt-px flex sm:col-start-3" style="grid-row: 74 / span 18">
-                                                        <a href="#" class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-blue-50 p-2 text-xs leading-5 hover:bg-blue-100">
-                                                            <p class="order-1 font-semibold text-blue-700">Breakfast</p>
-                                                            <p class="text-blue-500 group-hover:text-blue-700"><time datetime="2022-01-12T06:00">6:00 AM</time></p>
+                                                <ol class="col-start-1 col-end-2 row-start-1 grid grid-cols-1 sm:grid-cols-7 sm:pr-8" style="grid-template-rows: 1rem repeat(288, minmax(0, 1fr)) auto">
+                                                    <li class="relative mt-px flex sm:col-start-1" style="grid-row: 4 / span 56">
+                                                        <a href="#" class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-pink-50 p-2 text-xs leading-5 hover:bg-pink-100">
+                                                            <p class="order-1 font-semibold text-pink-700">Cognitive Computing</p>
+                                                            <p class="text-pink-500 group-hover:text-pink-700"><time datetime="2022-01-12T07:30">7:00 AM</time></p>
                                                         </a>
                                                     </li>
-                                                    <li class="relative mt-px flex sm:col-start-3" style="grid-row: 92 / span 30">
-                                                        <a href="#" class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-pink-50 p-2 text-xs leading-5 hover:bg-pink-100">
-                                                            <p class="order-1 font-semibold text-pink-700">Flight to Paris</p>
-                                                            <p class="text-pink-500 group-hover:text-pink-700"><time datetime="2022-01-12T07:30">7:30 AM</time></p>
+                                                    <li class="relative mt-px flex sm:col-start-1" style="grid-row: 58 / span 56">
+                                                        <a href="#" class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-green-50 p-2 text-xs leading-5 hover:bg-green-100">
+                                                            <p class="order-1 font-semibold text-green-700">Cloud Computing</p>
+                                                            <p class="text-green-500 group-hover:text-green-700"><time datetime="2022-01-12T07:30">7:00 AM</time></p>
+                                                        </a>
+                                                    </li>
+                                                    <li class="relative mt-px flex sm:col-start-1" style="grid-row: 113 / span 15">
+                                                        <a href="#" class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-red-50 p-2 text-xs leading-5 hover:bg-red-100">
+                                                            <p class="order-1 font-semibold text-red-700">Cloud Computing</p>
+                                                            <p class="text-red-500 group-hover:text-red-700"><time datetime="2022-01-12T07:30">7:00 AM</time></p>
                                                         </a>
                                                     </li>
                                                 </ol>
