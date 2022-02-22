93ae83bb58d43cbeb2dafe2fe6a47497ceb4c8be
Will <twitchifygamers@gmail.com>
2022-02-22 21:18:46 +0000

    Created forelse for viewing notifications

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -99,7 +99,7 @@
                 <div class="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
                     <div class="flex-1 min-w-0">
                         <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">
-                            Notifications
+                            Your notifications
                         </h1>
                     </div>
                     <div class="mt-4 flex sm:mt-0 sm:ml-4">
@@ -110,25 +110,34 @@
                     </div>
                 </div>
 
-                <ul role="list" class="divide-y divide-gray-200">
-                    <li class="relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-                        <div class="flex justify-between space-x-3">
-                            <div class="min-w-0 flex-1">
-                                <a href="#" class="block focus:outline-none">
-                                    <span class="absolute inset-0" aria-hidden="true"></span>
-                                    <p class="text-sm font-medium text-gray-900 truncate">Gloria Roberston</p>
-                                    <p class="text-sm text-gray-500 truncate">Velit placeat sit ducimus non sed</p>
-                                </a>
+                @forelse($notify as $n)
+                    <ul role="list" class="divide-y divide-gray-200">
+                        <li class="relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                            <div class="flex justify-between space-x-3">
+                                <div class="min-w-0 flex-1">
+                                    <a href="#" class="block focus:outline-none">
+                                        <span class="absolute inset-0" aria-hidden="true"></span>
+                                        <p class="text-sm font-medium text-gray-900 truncate">Gloria Roberston</p>
+                                        <p class="text-sm text-gray-500 truncate">Velit placeat sit ducimus non sed</p>
+                                    </a>
+                                </div>
+                                <time datetime="2021-01-27T16:35" class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">1d ago</time>
                             </div>
-                            <time datetime="2021-01-27T16:35" class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">1d ago</time>
-                        </div>
-                        <div class="mt-1">
-                            <p class="line-clamp-2 text-sm text-gray-600">Doloremque dolorem maiores assumenda dolorem facilis. Velit vel in a rerum natus facere. Enim rerum eaque qui facilis. Numquam laudantium sed id dolores omnis in. Eos reiciendis deserunt maiores et accusamus quod dolor.</p>
-                        </div>
-                    </li>
+                            <div class="mt-1">
+                                <p class="line-clamp-2 text-sm text-gray-600">Doloremque dolorem maiores assumenda dolorem facilis. Velit vel in a rerum natus facere. Enim rerum eaque qui facilis. Numquam laudantium sed id dolores omnis in. Eos reiciendis deserunt maiores et accusamus quod dolor.</p>
+                            </div>
+                        </li>
 
-
-                </ul>
+                    </ul>
+                @empty
+                    <div class="text-center pt-4">
+                        <svg class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path vector-effect="non-scaling-stroke" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
+                        </svg>
+                        <h3 class="mt-2 text-sm font-medium text-gray-900">No notifications</h3>
+                        <p class="mt-1 text-sm text-gray-500">Your all caught up!</p>
+                    </div>
+                @endforelse
             </main>
         </div>
     </div>
