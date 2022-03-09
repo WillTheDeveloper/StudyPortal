83882f820295237988ba12911d87fc77d3cadc69
Will <twitchifygamers@gmail.com>
2022-03-09 21:40:00 +0000

    Updated how the comments look

--- resources/views/communitypost.blade.php
+++ resources/views/communitypost.blade.php
@@ -218,42 +218,26 @@
                     </div>
                 </div>
 
-
-                <ul role="list" class="-mb-8">
+                <ul role="list" class="divide-y divide-gray-200 py-5">
                     @foreach($post->Comments->sortByDesc('created_at') as $p)
-                    <li>
-                        <div class="relative pb-8">
-                            <span class="absolute top-5 left-5 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
-                            <div class="relative flex items-start space-x-3">
-                                <div class="relative">
-                                    <img class="h-10 w-10 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white" src="https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80" alt="">
-
-                                    <span class="absolute -bottom-0.5 -right-1 bg-white rounded-tl px-0.5 py-px">
-                                      <!-- Heroicon name: solid/chat-alt -->
-                                      <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd" />
-                                      </svg>
-                                    </span>
-                                </div>
-                                <div class="min-w-0 flex-1">
-                                    <div>
-                                        <div class="text-sm">
-                                            <a href="{{ route('community.profile', $p->User->id) }}" class="font-medium text-gray-900">{{$p->User->name}}</a>
-                                        </div>
-                                        <p class="mt-0.5 text-sm text-gray-500">
-                                            Commented {{$p->created_at->DiffForHumans()}}
-                                        </p>
-                                    </div>
-                                    <div class="mt-2 text-sm text-gray-700">
-                                        <p>
-                                            {{$p->comment}}
-                                        </p>
-                                    </div>
-                                </div>
+                    <li class="relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                        <div class="flex justify-between space-x-3">
+                            <div class="min-w-0 flex-1">
+                                <a href="#" class="block focus:outline-none">
+                                    <span class="absolute inset-0" aria-hidden="true"></span>
+                                    <p class="text-sm font-medium text-gray-900 truncate">{{$p->comment}}</p>
+                                    <p class="text-sm text-gray-500 truncate">{{$p->User->name}}</p>
+                                </a>
                             </div>
+                            <time datetime="2021-01-27T16:35" class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">{{$p->created_at->diffForHumans()}}</time>
                         </div>
+                       {{-- <div class="mt-1">
+                            <p class="line-clamp-2 text-sm text-gray-600">{{$p->body}}</p>
+                        </div>--}}
                     </li>
                     @endforeach
+
+                    <!-- More messages... -->
                 </ul>
             </div>
         </div>
