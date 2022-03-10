55719f9dac43274869e29247d5a75490d1b359cd
Will <twitchifygamers@gmail.com>
2022-03-10 20:14:49 +0000

    Hide post actions if user doesn't have the perm

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -317,38 +317,43 @@
                                                                      tabindex="-1">
                                                                     <div class="py-1" role="none">
                                                                         <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                                                                        <a href="#"
-                                                                           class="text-gray-700 flex px-4 py-2 text-sm"
-                                                                           role="menuitem" tabindex="-1"
-                                                                           id="options-menu-0-item-0">
-                                                                            <!-- Heroicon name: solid/star -->
-                                                                            <svg class="mr-3 h-5 w-5 text-gray-400"
-                                                                                 xmlns="http://www.w3.org/2000/svg"
-                                                                                 viewBox="0 0 20 20"
-                                                                                 fill="currentColor" aria-hidden="true">
-                                                                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-                                                                            </svg>
-                                                                            <span>Edit</span>
-                                                                        </a>
-                                                                        <form action="{{ route('community.delete', $post->id) }}"
-                                                                              method="post">
-                                                                            @csrf
-                                                                            <a class="text-gray-700 flex px-4 py-2 text-sm"
+                                                                        @if ($post->user_id == auth()->id())
+                                                                            <a href="#"
+                                                                               class="text-gray-700 flex px-4 py-2 text-sm"
                                                                                role="menuitem" tabindex="-1"
-                                                                               id="options-menu-0-item-1">
-                                                                                <!-- Heroicon name: solid/code -->
+                                                                               id="options-menu-0-item-0">
+                                                                                <!-- Heroicon name: solid/star -->
                                                                                 <svg class="mr-3 h-5 w-5 text-gray-400"
                                                                                      xmlns="http://www.w3.org/2000/svg"
                                                                                      viewBox="0 0 20 20"
-                                                                                     fill="currentColor"
-                                                                                     aria-hidden="true">
-                                                                                    <path fill-rule="evenodd"
-                                                                                          d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
-                                                                                          clip-rule="evenodd"/>
+                                                                                     fill="currentColor" aria-hidden="true">
+                                                                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
                                                                                 </svg>
-                                                                                <button type="submit">Delete</button>
+                                                                                <span>Edit</span>
                                                                             </a>
-                                                                        </form>
+                                                                        @endif
+
+                                                                        @if (auth()->user()->is_admin)
+                                                                            <form action="{{ route('community.delete', $post->id) }}"
+                                                                                  method="post">
+                                                                                @csrf
+                                                                                <a class="text-gray-700 flex px-4 py-2 text-sm"
+                                                                                   role="menuitem" tabindex="-1"
+                                                                                   id="options-menu-0-item-1">
+                                                                                    <!-- Heroicon name: solid/code -->
+                                                                                    <svg class="mr-3 h-5 w-5 text-gray-400"
+                                                                                         xmlns="http://www.w3.org/2000/svg"
+                                                                                         viewBox="0 0 20 20"
+                                                                                         fill="currentColor"
+                                                                                         aria-hidden="true">
+                                                                                        <path fill-rule="evenodd"
+                                                                                              d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
+                                                                                              clip-rule="evenodd"/>
+                                                                                    </svg>
+                                                                                    <button type="submit">Delete</button>
+                                                                                </a>
+                                                                            </form>
+                                                                        @endif
                                                                         <a href="#"
                                                                            class="text-gray-700 flex px-4 py-2 text-sm"
                                                                            role="menuitem" tabindex="-1"
