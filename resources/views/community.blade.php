a9a70148100219bfe807ac8eb55b1d23959a14e6
Will <twitchifygamers@gmail.com>
2022-03-11 19:59:57 +0000

    Updated subheaders inside community

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -217,11 +217,11 @@
                                     <label for="question-tabs" class="sr-only">Select a tab</label>
                                     <select id="question-tabs"
                                             class="block w-full rounded-md border-gray-300 text-base font-medium text-gray-900 shadow-sm focus:border-rose-500 focus:ring-rose-500">
-                                        <option selected>Recent</option>
+                                        <option selected>Latest</option>
 
-                                        <option>Most Liked</option>
+                                        <option>Highest comments</option>
 
-                                        <option>Most Answers</option>
+                                        <option>Most Views</option>
                                     </select>
                                 </div>
                                 <div class="hidden sm:block">
@@ -230,21 +230,21 @@
                                         <!-- Current: "text-gray-900", Default: "text-gray-500 hover:text-gray-700" -->
                                         <a href="#" aria-current="page"
                                            class="text-gray-900 rounded-l-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
-                                            <span>Recent</span>
+                                            <span>Latest</span>
                                             <span aria-hidden="true"
                                                   class="bg-rose-500 absolute inset-x-0 bottom-0 h-0.5"></span>
                                         </a>
 
                                         <a href="#"
                                            class="text-gray-500 hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
-                                            <span>Most Liked</span>
+                                            <span>Highest comments</span>
                                             <span aria-hidden="true"
                                                   class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
                                         </a>
 
                                         <a href="#"
                                            class="text-gray-500 hover:text-gray-700 rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
-                                            <span>Most Answers</span>
+                                            <span>Most Views</span>
                                             <span aria-hidden="true"
                                                   class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
                                         </a>
