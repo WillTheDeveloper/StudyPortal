64efe6a791ed08525eee4090a71814b6d14162a5
Will <twitchifygamers@gmail.com>
2022-03-11 12:29:38 +0000

    Updated the main dashboard and removed old stuff

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -275,9 +275,9 @@
                                     <table class="min-w-full divide-y divide-gray-200">
                                         <thead>
                                         <tr>
-                                            <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Transaction</th>
-                                            <th class="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Amount</th>
-                                            <th class="hidden px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider md:block">Status</th>
+                                            <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Activity</th>
+                                            {{--<th class="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Amount</th>
+                                            <th class="hidden px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider md:block">Status</th>--}}
                                             <th class="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Date</th>
                                         </tr>
                                         </thead>
@@ -288,23 +288,23 @@
                                                 <div class="flex">
                                                     <a href="#" class="group inline-flex space-x-2 truncate text-sm">
                                                         <!-- Heroicon name: solid/cash -->
-                                                        <svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                        {{--<svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                             <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
-                                                        </svg>
+                                                        </svg>--}}
                                                         <p class="text-gray-500 truncate group-hover:text-gray-900">
                                                             {{json_encode($notif->data['user'])}} posted {{json_encode($notif->data['title'])}}</p>
                                                     </a>
                                                 </div>
                                             </td>
-                                            <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
+                                            {{--<td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
                                                 <span class="text-gray-900 font-medium">$20,000 </span>
                                                 USD
                                             </td>
                                             <td class="hidden px-6 py-4 whitespace-nowrap text-sm text-gray-500 md:block">
                                                 <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800 capitalize"> success </span>
-                                            </td>
+                                            </td>--}}
                                             <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-                                                <time datetime="2020-07-11">{{$notif->created_at->format('D n M')}}</time>
+                                                <time datetime="2020-07-11">{{$notif->created_at->format('h:m - D n M')}}</time>
                                             </td>
                                         </tr>
                                         @endforeach
