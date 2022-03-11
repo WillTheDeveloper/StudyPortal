aa6d5af0c036fbd1f3c3365ab3aff2aaa2148438
Will <twitchifygamers@gmail.com>
2022-03-11 12:13:34 +0000

    Update email to view telescope

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -64,7 +64,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                'will@teamatlas.cloud'
+                'willthedeveloper13@gmail.com'
             ]);
         });
     }
