fde5ad9dce06faff9c7b08d8a60985085b403c97
Will <twitchifygamers@gmail.com>
2022-03-13 17:31:41 +0000

    Removed gap above the list because it's bad

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -116,7 +116,7 @@
 
                 <!-- This example requires Tailwind CSS v2.0+ -->
                 <div class="px-4 sm:px-6 lg:px-8">
-                    <div class="mt-8 flex flex-col">
+                    <div class="flex flex-col">
                         <div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
                             <div class="inline-block min-w-full py-2 align-middle">
                                 <div class="shadow-sm ring-1 ring-black ring-opacity-5">
