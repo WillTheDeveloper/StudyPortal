1c63cb4c1566437a8451e4f85de6371371e8edb6
Will <twitchifygamers@gmail.com>
2022-03-16 08:07:12 +0000

    Created request validation for timetable

---
+++ app/Http/Requests/AddLessonToTimetable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddLessonToTimetable extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'start' => ['required'],
+            'end' => ['required'],
+            'subject_id' => ['required', 'integer'],
+            'weekday' => ['string', 'required'],
+            'room' => ['nullable', 'string']
+        ];
+    }
+}
