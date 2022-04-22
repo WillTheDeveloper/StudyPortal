9cac5b62ad58aea015930828dddaf813547b095c
Will <twitchifygamers@gmail.com>
2022-04-22 18:25:18 +0100

    Created the report migration

---
+++ database/migrations/2022_04_22_172222_create_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('resolved');
+            $table->integer('post_id');
+            $table->integer('user_id');
+            $table->string('reason');
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+};
