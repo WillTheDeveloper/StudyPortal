66e3b5bf004566f4aee62a062f70b4383d6be55d
Will <twitchifygamers@gmail.com>
2022-04-29 10:39:03 +0100

    Created report factory

---
+++ database/factories/ReportFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\Report;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Report>
+ */
+class ReportFactory extends Factory
+{
+
+    protected $model = Report::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'resolved' => $this->faker->boolean,
+            'post_id' => Post::query()->get('id')->random(),
+            'user_id' => User::query()->get('id')->random(),
+            'reason' => 'Testing Only',
+            'severity' => 'High',
+            'comment' => $this->faker->text(25)
+        ];
+    }
+}
