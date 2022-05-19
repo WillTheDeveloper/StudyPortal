2af808183501ac539eaa89fbc4e595c9f758a4fd
Will <twitchifygamers@gmail.com>
2022-05-19 20:19:23 +0100

    Fixed link typo on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -141,7 +141,7 @@
                     </a>
                 </div>
                 <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                    <a href="{{route('Blog')}}" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-grape-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
+                    <a href="{{route('blog')}}" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-grape-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
                         Blog
                     </a>
                 </div>
