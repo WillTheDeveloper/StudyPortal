0486066e68de755270c7d997f96041055996e503
Will <twitchifygamers@gmail.com>
2022-05-22 10:39:04 +0100

    Added validation messages to profile and moved the error output

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -142,31 +142,31 @@
                                     </button>
                                 </form>
 
-                                @if($errors->all())
-                                    <div class="rounded-md bg-red-50 p-4">
-                                        <div class="flex">
-                                            <div class="flex-shrink-0">
-                                                <!-- Heroicon name: solid/x-circle -->
-                                                <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
-                                                </svg>
-                                            </div>
-                                            <div class="ml-3">
-                                                <h3 class="text-sm font-medium text-red-800">There were {{$errors->count()}} errors with your submission</h3>
-                                                <div class="mt-2 text-sm text-red-700">
-                                                    <ul role="list" class="list-disc pl-5 space-y-1">
-                                                        @foreach($errors->all() as $e)
-                                                            <li>{{$e}}</li>
-                                                        @endforeach
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                @endif
-
                             </div>
                         </div>
+
+                        @if($errors->all())
+                            <div class="rounded-md bg-red-50 p-4">
+                                <div class="flex">
+                                    <div class="flex-shrink-0">
+                                        <!-- Heroicon name: solid/x-circle -->
+                                        <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    <div class="ml-3">
+                                        <h3 class="text-sm font-medium text-red-800">There were {{$errors->count()}} errors with your submission</h3>
+                                        <div class="mt-2 text-sm text-red-700">
+                                            <ul role="list" class="list-disc pl-5 space-y-1">
+                                                @foreach($errors->all() as $e)
+                                                    <li>{{$e}}</li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
 
                     <form class="space-y-8 divide-y divide-gray-200" action="{{ route('profile.update') }}"
                           method="post">
