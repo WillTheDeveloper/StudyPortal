50ac2820b1a1f6611e496894ebbb9b2d421d38f0
Will <twitchifygamers@gmail.com>
2022-05-28 11:48:44 +0100

    Added data from DB to discussions

--- resources/views/groupdiscussions.blade.php
+++ resources/views/groupdiscussions.blade.php
@@ -88,6 +88,7 @@
                     </div>
                 </div>
             </div>
+        </div>
     <div class="mt-4">
         <h1 class="sr-only">Recent questions</h1>
         <ul role="list" class="space-y-4">
@@ -101,7 +102,7 @@
                             </div>
                             <div class="min-w-0 flex-1">
                                 <p class="text-sm font-medium text-gray-900">
-                                    <a href="#" class="hover:underline">Dries Vincent</a>
+                                    <a href="{{route('community.profile', $d->User->id)}}" class="hover:underline">{{$d->User->name}}</a>
                                 </p>
                                 <p class="text-sm text-gray-500">
                                     <a href="#" class="hover:underline">
@@ -160,57 +161,10 @@
                                 </div>
                             </div>
                         </div>
-                        <h2 id="question-title-81614" class="mt-4 text-base font-medium text-gray-900">What would you have done differently if you ran Jurassic Park?</h2>
+                        <h2 id="question-title-81614" class="mt-4 text-base font-medium text-gray-900">{{$d->title}}</h2>
                     </div>
                     <div class="mt-2 text-sm text-gray-700 space-y-4">
-                        <p>Jurassic Park was an incredible idea and a magnificent feat of engineering, but poor protocols and a disregard for human safety killed what could have otherwise been one of the best businesses of our generation.</p>
-                        <p>Ultimately, I think that if you wanted to run the park successfully and keep visitors safe, the most important thing to prioritize would be&hellip;</p>
-                    </div>
-                    <div class="mt-6 flex justify-between space-x-8">
-                        <div class="flex space-x-6">
-                    <span class="inline-flex items-center text-sm">
-                      <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
-                        <!-- Heroicon name: solid/thumb-up -->
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                          <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
-                        </svg>
-                        <span class="font-medium text-gray-900">29</span>
-                        <span class="sr-only">likes</span>
-                      </button>
-                    </span>
-                            <span class="inline-flex items-center text-sm">
-                      <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
-                        <!-- Heroicon name: solid/chat-alt -->
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                          <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd" />
-                        </svg>
-                        <span class="font-medium text-gray-900">11</span>
-                        <span class="sr-only">replies</span>
-                      </button>
-                    </span>
-                            <span class="inline-flex items-center text-sm">
-                      <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
-                        <!-- Heroicon name: solid/eye -->
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                          <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                          <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                        </svg>
-                        <span class="font-medium text-gray-900">2.7k</span>
-                        <span class="sr-only">views</span>
-                      </button>
-                    </span>
-                        </div>
-                        <div class="flex text-sm">
-                    <span class="inline-flex items-center text-sm">
-                      <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
-                        <!-- Heroicon name: solid/share -->
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                          <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
-                        </svg>
-                        <span class="font-medium text-gray-900">Share</span>
-                      </button>
-                    </span>
-                        </div>
+                        <p>{{$d->body}}</p>
                     </div>
                 </article>
             </li>
