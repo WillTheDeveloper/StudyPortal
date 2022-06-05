992a7de1ce0e7dcce51c3baf0a6fe7cfc99a7b05
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-06-05 17:10:22 +0000

    Added locked status for chats on discussions

--- resources/views/discussionschat.blade.php
+++ resources/views/discussionschat.blade.php
@@ -164,31 +164,57 @@
                         </div>
                     </article>
 
-                <div class="pt-5 px-10 pb-5">
-                    <form action="" method="post" class="relative">
-                        @csrf
-                        <div class="border border-gray-300 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
-                            <label for="message" class="sr-only">Description</label>
-                            <textarea rows="2" name="message" id="message" class="block w-full border-0 py-0 pt-2 resize-none placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="Write a description..."></textarea>
+                @if($main->locked == 0)
+                    <div class="pt-5 px-10 pb-5">
+                        <form action="" method="post" class="relative">
+                            @csrf
+                            <div class="border border-gray-300 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+                                <label for="message" class="sr-only">Description</label>
+                                <textarea rows="2" name="message" id="message" class="block w-full border-0 py-0 pt-2 resize-none placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="Write a description..."></textarea>
 
-                            <!-- Spacer element to match the height of the toolbar -->
-                            <div aria-hidden="true">
-                                <div class="py-2">
-                                    <div class="h-9"></div>
+                                <!-- Spacer element to match the height of the toolbar -->
+                                <div aria-hidden="true">
+                                    <div class="py-2">
+                                        <div class="h-9"></div>
+                                    </div>
+                                    <div class="h-px"></div>
                                 </div>
-                                <div class="h-px"></div>
                             </div>
-                        </div>
 
-                        <div class="absolute bottom-0 inset-x-px">
-                            <div class="border-t border-gray-200 px-2 py-2 flex justify-between items-center space-x-3 sm:px-3">
+                            <div class="absolute bottom-0 inset-x-px">
+                                <div class="border-t border-gray-200 px-2 py-2 flex justify-between items-center space-x-3 sm:px-3">
+                                    <div class="flex-shrink-0">
+                                        <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Comment</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                @else
+
+                    <div class="pt-4 pl-10 pr-10 pb-4">
+                        <!-- This example requires Tailwind CSS v2.0+ -->
+                        <div class="rounded-md bg-yellow-50 p-4">
+                            <div class="flex">
                                 <div class="flex-shrink-0">
-                                    <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Comment</button>
+                                    <!-- Heroicon name: solid/exclamation -->
+                                    <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                                <div class="ml-3">
+                                    <h3 class="text-sm font-medium text-yellow-800">Discussion status</h3>
+                                    <div class="mt-2 text-sm text-yellow-700">
+                                        <p>The tutor who created this chat has locked the chat so no replies can be sent temporarily.</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </form>
-                </div>
+
+                    </div>
+
+                @endif
 
 
 
