37f813511e932809d857e8e353e71a83bd2b848b
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-06-07 18:05:14 +0000

    Added tags to webhooks table

--- resources/views/webhooks.blade.php
+++ resources/views/webhooks.blade.php
@@ -102,10 +102,10 @@
                 <div class="sm:flex sm:items-center">
                     <div class="sm:flex-auto">
                         <h1 class="text-xl font-semibold text-gray-900">Webhooks</h1>
-                        <p class="mt-2 text-sm text-gray-700">A table of placeholder stock market data that does not make any sense.</p>
+                        <p class="mt-2 text-sm text-gray-700">A table with all your webhooks along with status tags.</p>
                     </div>
                     <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                        <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Export</button>
+                        <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">New</button>
                     </div>
                 </div>
                 <div class="mt-8 flex flex-col">
