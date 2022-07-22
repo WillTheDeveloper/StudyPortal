4c24ea8695cd91ae4116753115bb08aea9093bf6
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-22 20:58:26 +0100

    Added area to show bio

--- resources/views/communityuser.blade.php
+++ resources/views/communityuser.blade.php
@@ -144,7 +144,7 @@
                 </h1>
             </div>
 
-            <dl class="mt-3 mb-3 grid grid-cols-1 gap-5 sm:grid-cols-3">
+            <dl class="mt-3 mb-1 grid grid-cols-1 gap-5 sm:grid-cols-3">
                 <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
                     <dt class="text-sm font-medium text-gray-500 truncate">
                         Posts
@@ -176,7 +176,29 @@
         </div>
     </div>
 
-    <!-- This example requires Tailwind CSS v2.0+ -->
+
+            @isset($user->bio)
+            <!-- This example requires Tailwind CSS v2.0+ -->
+            <section class="overflow-hidden">
+                <div class="relative max-w-7xl mx-auto pt-20 pb-12 px-4 sm:px-6 lg:px-8 lg:py-20">
+                    <div class="relative lg:flex lg:items-center">
+                        <div class="relative lg:ml-10">
+                            <svg class="absolute top-0 left-0 transform -translate-x-8 -translate-y-24 h-36 w-36 text-indigo-200 opacity-50" stroke="currentColor" fill="none" viewBox="0 0 144 144" aria-hidden="true">
+                                <path stroke-width="2" d="M41.485 15C17.753 31.753 1 59.208 1 89.455c0 24.664 14.891 39.09 32.109 39.09 16.287 0 28.386-13.03 28.386-28.387 0-15.356-10.703-26.524-24.663-26.524-2.792 0-6.515.465-7.446.93 2.327-15.821 17.218-34.435 32.11-43.742L41.485 15zm80.04 0c-23.268 16.753-40.02 44.208-40.02 74.455 0 24.664 14.891 39.09 32.109 39.09 15.822 0 28.386-13.03 28.386-28.387 0-15.356-11.168-26.524-25.129-26.524-2.792 0-6.049.465-6.98.93 2.327-15.821 16.753-34.435 31.644-43.742L121.525 15z" />
+                            </svg>
+                            <blockquote class="relative">
+                                <div class="text-2xl leading-9 font-medium text-gray-900">
+                                    <p>{{$user->bio}}</p>
+                                </div>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            @endisset
+
+
+            <!-- This example requires Tailwind CSS v2.0+ -->
     <div class="bg-gray-100">
         <div class="max-w-2xl py-3 px-4 grid  gap-y-2 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 ">
 
