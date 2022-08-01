f5a1f9ffbfdf698aad713412785dd73e00d29a0d
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-08-01 22:07:04 +0100

    Added response input box on blog read page

--- resources/views/blogread.blade.php
+++ resources/views/blogread.blade.php
@@ -188,6 +188,32 @@
         </div>
 
         @if ($content->replies)
+
+            <div class="flex items-start space-x-4 pt-4">
+                <div class="flex-shrink-0">
+                    <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                </div>
+                <div class="min-w-0 flex-1">
+                    <form action="#" class="relative">
+                        <div class="border border-gray-300 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+                            <label for="comment" class="sr-only">Add your comment</label>
+                            <textarea rows="3" name="comment" id="comment" class="block w-full py-3 border-0 resize-none focus:ring-0 sm:text-sm" placeholder="Add your comment..."></textarea>
+
+                            <!-- Spacer element to match the height of the toolbar -->
+                            <div class="py-2" aria-hidden="true">
+                                <!-- Matches height of button in toolbar (1px border + 36px content height) -->
+                                <div class="py-px">
+                                    <div class="h-9"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex-shrink-0">
+                                <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Post</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
         <div class="bg-white pt-16">
             <div>
                 <h2 class="sr-only">Responses</h2>
