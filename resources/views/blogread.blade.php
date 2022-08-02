7648005a1feb3610d864ea0a10f798a1076d658a
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-08-02 09:53:27 +0100

    Updated message for none auth users when viewing enabled responses on blog

--- resources/views/blogread.blade.php
+++ resources/views/blogread.blade.php
@@ -189,6 +189,7 @@
 
         @if ($content->replies)
 
+            @auth()
             <div class="flex items-start space-x-4 pt-4">
                 <div class="flex-shrink-0">
                     <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
@@ -214,8 +215,31 @@
                     </form>
                 </div>
             </div>
+            @endauth()
 
-        <div class="bg-white pt-16">
+            @guest()
+                    <!-- This example requires Tailwind CSS v2.0+ -->
+                <div class="pt-4">
+                    <div class="rounded-md bg-yellow-50 p-4">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <!-- Heroicon name: solid/exclamation -->
+                                <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="ml-3">
+                                <h3 class="text-sm font-medium text-yellow-800">Authentication required</h3>
+                                <div class="mt-2 text-sm text-yellow-700">
+                                    <p>Responses for this blog post are enabled but require authentication in order to respond to it. <a href="{{route('login')}}" class="underline">You can login here.</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endguest()
+
+                    <div class="bg-white pt-16">
             <div>
                 <h2 class="sr-only">Responses</h2>
 
