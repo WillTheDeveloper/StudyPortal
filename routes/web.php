4aa56a9d2cae88183a8675fcb170c8f9ad147739
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-08-18 12:06:44 +0100

    Deleted old api stuff
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -400,22 +400,6 @@ Route::post('/blog/{slug}/response/create', [Blog::class, 'response'])
     ->middleware(['auth', 'verified'])
     ->name('blog.response.create');
 
-//API GET ROUTES
-Route::prefix('api')->group(function () {
-    Route::get('/user', function () {
-        return new UserResource(\App\Models\User::query()->findOrFail(auth()->id()));
-    })->middleware('auth:sanctum')->name('api.user');
-    Route::get('/post/{id}', function ($id) {
-        return new PostResource(Post::query()->findOrFail($id));
-    })->middleware('auth:sanctum')->name('api.post.id');
-    Route::get('/posts', function () {
-        return new PostCollection(Post::all());
-    })->middleware('auth:sanctum')->name('api.post.collection');
-    Route::get('/user/posts', function () {
-        return new PostCollection(Post::all()->where('user_id', auth()->id()));
-    })->middleware('auth:sanctum')->name('api.post.user');
-});
-
 //API POST ROUTES
 Route::post('/keys/create', function (Request $request) {
     $token = $request->user()->createToken($request->token_name);
