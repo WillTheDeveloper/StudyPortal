4a17f9d52ae054d94ae0450ebe0cb67c5de4f7eb
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-25 19:57:11 +0100

    Removed old api stuff from api.php
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,2 @@
 <?php
 
-use App\Http\Resources\UserResource;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/user/current', function () {
-    return new UserResource(User::query()->findOrFail(auth()->id()));
-})->middleware('auth:sanctum');
-
-Route::get('/user/{id}', function ($id) {
-    return new UserResource(User::query()->findOrFail($id));
-})->middleware('auth:sanctum');
