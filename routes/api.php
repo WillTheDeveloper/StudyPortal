b86588ee7f9af4a9c355647a4b783065d187628c
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-25 20:22:27 +0100

    Added auth check and more to user resource
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/users', function () {
     return new \App\Http\Resources\UserCollection(\App\Models\User::all());
-});
+})->middleware('auth:sanctum');
 Route::get('/user/{id}', function ($id) {
     return new \App\Http\Resources\UserResource(\App\Models\User::findOrFail($id));
-});
+})->middleware(['auth:sanctum', 'admin']);
