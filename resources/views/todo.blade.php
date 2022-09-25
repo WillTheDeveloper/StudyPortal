92dc9e5a9e4da841bc4620bbd11d30bc87db2330
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-25 15:59:39 +0100

    Removed tick boxes from list and heading
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -304,19 +304,11 @@
                     <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                             <div class="relative overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                                <!-- Selected row actions, only show when rows are selected. -->
-                                <div class="absolute top-0 left-12 flex h-12 items-center space-x-3 bg-gray-50 sm:left-16">
-                                    <button type="button" class="inline-flex items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30">Bulk edit</button>
-                                    <button type="button" class="inline-flex items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30">Delete all</button>
-                                </div>
 
                                 <table class="min-w-full table-fixed divide-y divide-gray-300">
                                     <thead class="bg-gray-50">
                                     <tr>
-                                        <th scope="col" class="relative w-12 px-6 sm:w-16 sm:px-8">
-                                            <input type="checkbox" class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 sm:left-6">
-                                        </th>
-                                        <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">Name</th>
+                                        <th scope="col" class="min-w-[12rem] pl-3 py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">Name</th>
                                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
                                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
                                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
@@ -328,14 +320,8 @@
                                     <tbody class="divide-y divide-gray-200 bg-white">
                                     <!-- Selected: "bg-gray-50" -->
                                     <tr>
-                                        <td class="relative w-12 px-6 sm:w-16 sm:px-8">
-                                            <!-- Selected row marker, only show when row is selected. -->
-                                            <div class="absolute inset-y-0 left-0 w-0.5 bg-indigo-600"></div>
-
-                                            <input type="checkbox" class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 sm:left-6">
-                                        </td>
                                         <!-- Selected: "text-indigo-600", Not Selected: "text-gray-900" -->
-                                        <td class="whitespace-nowrap py-4 pr-3 text-sm font-medium text-gray-900">Lindsay Walton</td>
+                                        <td class="whitespace-nowrap py-4 pr-3 pl-3 text-sm font-medium text-gray-900">Lindsay Walton</td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Front-end Developer</td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">lindsay.walton@example.com</td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
