929f6294f83e40d803c3fdc8604e900e7370cafb
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-26 17:45:19 +0100

    Created note assets for the api
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

---
+++ app/Http/Resources/NoteResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NoteResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'note' => $this->note,
+            'user' => $this->User->name,
+            'private' => $this->private,
+            'created' => $this->created_at,
+            'updated' => $this->updated_at
+        ];
+    }
+}
