1d55454b5593c81c6b2a5af92c23e7f7b39826b3
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-26 10:56:05 +0100

    Changed colour for response headers
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/api/v1/posts.blade.php
+++ resources/views/api/v1/posts.blade.php
@@ -276,7 +276,7 @@
                                             {{--<div class="text-gray-500">Optimization</div>--}}
                                         </td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            <span class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">No</span>
+                                            <span class="inline-flex rounded-full bg-red-100 px-2 text-xs font-semibold leading-5 text-red-800">No</span>
                                         </td>
                                     </tr>
 
