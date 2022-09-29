52e5292ab6f00594050efebf9be426a3795598bf
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-29 22:02:20 +0100

    Added documentation links to first few api endpoints as part of json
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- app/Http/Resources/CommentCollection.php
+++ app/Http/Resources/CommentCollection.php
@@ -17,7 +17,7 @@ class CommentCollection extends ResourceCollection
         return [
             'data' => $this->collection,
             'links' => [
-                'self' => 'link-value'
+                'documentation' => route('docs.v1.comments')
             ]
         ];
     }
--- app/Http/Resources/DiscussionCollection.php
+++ app/Http/Resources/DiscussionCollection.php
@@ -17,7 +17,7 @@ class DiscussionCollection extends ResourceCollection
         return [
             'data' => $this->collection,
             'links' => [
-                'self' => 'link-value'
+                'documentation' => route('docs.v1.discussion')
             ]
         ];
     }
