f8bedd881be3b1f65fa565f1de7f5697828eac21
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-29 21:34:31 +0100

    Added name and email to updatable profile
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -173,6 +173,31 @@
                         @csrf
                         <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
                             <div>
+
+                                <div class="space-y-6 sm:space-y-5">
+                                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                        <label for="first-name"
+                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                            Full Name
+                                        </label>
+                                        <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                            <input type="text" name="first-name" id="first-name"
+                                                   autocomplete="given-name" value="{{auth()->user()->name}}"
+                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                        </div>
+                                    </div>
+
+                                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                        <label for="email"
+                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                            Email address
+                                        </label>
+                                        <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                            <input id="email" name="email" type="email" autocomplete="email" value="{{auth()->user()->email}}"
+                                                   class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md">
+                                        </div>
+                                    </div>
+
                                 <div class="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
                                     <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                         <label for="username"
@@ -256,114 +281,6 @@
                                 </div>
                             </div>
 
-                            <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-                                <div>
-                                    <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                        Personal Information
-                                    </h3>
-                                    <p class="mt-1 max-w-2xl text-sm text-gray-500">
-                                        Use a permanent address where you can receive mail.
-                                    </p>
-                                </div>
-                                <div class="space-y-6 sm:space-y-5">
-                                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                        <label for="first-name"
-                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                            First name
-                                        </label>
-                                        <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="first-name" id="first-name"
-                                                   autocomplete="given-name"
-                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
-                                        </div>
-                                    </div>
-
-                                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                        <label for="last-name"
-                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                            Last name
-                                        </label>
-                                        <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="last-name" id="last-name"
-                                                   autocomplete="family-name"
-                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
-                                        </div>
-                                    </div>
-
-                                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                        <label for="email"
-                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                            Email address
-                                        </label>
-                                        <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input id="email" name="email" type="email" autocomplete="email"
-                                                   class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md">
-                                        </div>
-                                    </div>
-
-                                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                        <label for="country"
-                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                            Country / Region
-                                        </label>
-                                        <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <select id="country" name="country" autocomplete="country"
-                                                    class="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
-                                                <option>United States</option>
-                                                <option>Canada</option>
-                                                <option>Mexico</option>
-                                            </select>
-                                        </div>
-                                    </div>
-
-                                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                        <label for="street-address"
-                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                            Street address
-                                        </label>
-                                        <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="street-address" id="street-address"
-                                                   autocomplete="street-address"
-                                                   class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md">
-                                        </div>
-                                    </div>
-
-                                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                        <label for="city"
-                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                            City
-                                        </label>
-                                        <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="city" id="city"
-                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
-                                        </div>
-                                    </div>
-
-                                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                        <label for="state"
-                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                            State / Province
-                                        </label>
-                                        <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="state" id="state"
-                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
-                                        </div>
-                                    </div>
-
-                                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                        <label for="zip"
-                                               class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                            ZIP / Postal
-                                        </label>
-                                        <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                            <input type="text" name="zip" id="zip" autocomplete="postal-code"
-                                                   class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="pt-5">
                             <div class="flex justify-end">
                                 <button type="button"
@@ -382,6 +299,5 @@
                 </div>
             </main>
         </div>
-    </div>
     </div>
 </x-app-layout>
