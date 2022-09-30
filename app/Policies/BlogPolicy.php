60ff05fed6b1b2e3ce2b49023c9b0824fce7de7b
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-30 18:47:23 +0100

    Created most model policies
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

---
+++ app/Policies/BlogPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Blog;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class BlogPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Blog $blog)
+    {
+        //
+    }
+}
