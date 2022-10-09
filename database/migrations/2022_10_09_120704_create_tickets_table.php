5cb619a81486164ef74186ccd4777c57f20ba683
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-09 13:20:43 +0100

    Created ticket and message assets
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

---
+++ database/migrations/2022_10_09_120704_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->integer('student_id');
+            $table->integer('tutor_id');
+            $table->string('question');
+            $table->integer('subject_id');
+            $table->text('details')->nullable(true);
+            $table->enum('status', ['new', 'active', 'cancelled', 'completed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
