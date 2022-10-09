225d01830e934a5f4ad84f72ca640e2e545c8716
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-09 14:22:45 +0100

    Created views and added things to ticket controller to do epic things
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- app/Http/Controllers/Ticket.php
+++ app/Http/Controllers/Ticket.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Ticket extends Controller
 {
-    //
+    public function switch(Request $request)
+    {
+        switch ($request->user()->is_tutor or $request->user()->is_admin)
+        {
+            case true:
+                $this->tutorview();
+                break;
+            case false:
+                $this->studentview();
+                break;
+        }
+    }
+
+    public function studentview(Auth $auth)
+    {
+        return view('studenttickets', [
+            'tickets' => \App\Models\Ticket::query()
+                ->where('student_id', $auth->id())
+                ->paginate(20)
+        ]);
+    }
+
+    public function tutorview(Auth $auth)
+    {
+        return view('tutortickets', [
+            'tickets' => \App\Models\Ticket::query()
+                ->where('tutor_id', $auth->id())
+                ->paginate(20)
+        ]);
+    }
+
+    public function create(Auth $auth)
+    {
+        return view('createticket', [
+            'subjects' => Subject::query()
+                ->whereBelongsTo(\App\Models\User::find($auth->id()))
+                ->select('subjects.subject')
+        ]);
+    }
+
+    public function new(Request $request)
+    {
+
+    }
+
+    public function viewticket($id)
+    {
+
+    }
 }
