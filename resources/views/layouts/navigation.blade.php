95d3a3d73fc6afa30a47b8a59e42e2245f644c29
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-09 15:25:01 +0100

    Added tickets links to navigation bar on auth
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -104,6 +104,10 @@
                         {{ __('Community') }}
                     </x-responsive-nav-link>
 
+                    <x-responsive-nav-link :href="route('tickets')" :active="request()->routeIs('tickets')">
+                        {{ __('Tickets') }}
+                    </x-responsive-nav-link>
+
                     <x-responsive-nav-link :href="route('profile')" :active="request()->routeIs('profile')">
                         {{ __('Profile') }}
                     </x-responsive-nav-link>
@@ -287,6 +291,10 @@
 
                 <x-responsive-nav-link :href="route('community')" :active="request()->routeIs('community')">
                     {{ __('Community') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('tickets')" :active="request()->routeIs('tickets')">
+                    {{ __('Tickets') }}
                 </x-responsive-nav-link>
 
                 <x-responsive-nav-link :href="route('profile')" :active="request()->routeIs('profile')">
