5d5a2f9c0a3f5bac988a95adb786a89b0f035f4b
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-09 16:07:27 +0100

    Created mailable for new tickets
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

---
+++ resources/views/mail/ticket/ticketcreated.blade.php
@@ -0,0 +1,26 @@
+@component('mail::message')
+# Ticket successfully created
+
+Hello, {{$data->Student()->name}}.
+
+Your ticket has been created.
+
+**Question:** {{$data->question}}
+
+**Details:** {{$data->details}}
+
+**Tutor:** {{$data->Tutor()->name}}
+
+**Status:** {{Str::ucfirst($data->status)}}
+
+@component('mail::button', ['url' => route('ticket.id', $data->id)])
+Open ticket
+@endcomponent
+
+For reference, your ticket identifier is: {{$data->id}}
+
+The incorrect use of the ticketing system may lead to your access to the feature being revoked. Please use it for its intended use.
+
+Thanks,<br>
+Study Portal Support Ticketing System
+@endcomponent
