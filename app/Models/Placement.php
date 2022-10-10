62fe210274d40a560a3431efb9bc5568704b5c60
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-10 14:59:36 +0100

    Created assets for placements
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

---
+++ app/Models/Placement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Placement extends Model
+{
+    use HasFactory;
+}
