904d3b6d265e582062e468a7cdd5ecb95ca58cc1
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-10 12:14:35 +0100

    Added to message controller to reply to tickets
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- app/Http/Controllers/Message.php
+++ app/Http/Controllers/Message.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class Message extends Controller
 {
-    //
+    public function create($ticket, Request $request)
+    {
+        \App\Models\Message::query()->create([
+            'ticket_id' => $ticket,
+            'user_id' => auth()->id(),
+            'message' => $request->input('message')
+        ]);
+
+        return redirect(route('ticket.id', $ticket));
+    }
 }
