91247a1f4a2562f11fb4024f71a99cdb3a80e816
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-10 11:49:10 +0100

    Added comment area at the bottom of ticket feed
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/ticket.blade.php
+++ resources/views/ticket.blade.php
@@ -232,6 +232,25 @@
             </div>
 
 
+            <div class="flex items-start space-x-4 px-6 pt-8">
+                <div class="flex-shrink-0">
+                    <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                </div>
+                <div class="min-w-0 flex-1">
+                    <form action="#" class="relative">
+                        <div class="overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+                            <label for="comment" class="sr-only">Add your comment</label>
+                            <textarea rows="3" name="comment" id="comment" class="block w-full resize-none border-0 py-3 focus:ring-0 sm:text-sm" placeholder="Add your comment..."></textarea>
+
+                            <div class="flex-shrink-0 px-2 py-2">
+                                <button type="submit" class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Post</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+
 
         </div>
     </div>
