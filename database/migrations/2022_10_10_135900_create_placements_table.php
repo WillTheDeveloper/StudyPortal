d4b5e0beac8b50b592394965bb62256164b2e2e2
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-11 11:01:05 +0100

    Updated placements to use uuids as pk
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- database/migrations/2022_10_10_135900_create_placements_table.php
+++ database/migrations/2022_10_10_135900_create_placements_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('placements', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->integer('user_id');
             $table->text('location');
             $table->text('company');
