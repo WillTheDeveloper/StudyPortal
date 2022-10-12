880d50b672236e792da214961a24046522136dcb
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-12 14:30:22 +0100

    Removed unnessasary community title thing
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -97,13 +97,13 @@
             </div>
             <main class="flex-1">
                 <!-- Page title & actions -->
-                <div class="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
+                {{--<div class="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
                     <div class="flex-1 min-w-0">
                         <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">
                             Community
                         </h1>
                     </div>
-                    {{--<div class="mt-4 flex sm:mt-0 sm:ml-4">
+                    --}}{{--<div class="mt-4 flex sm:mt-0 sm:ml-4">
                         <button type="button"
                                 class="order-1 ml-3 inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-0 sm:ml-0">
                             Share
@@ -112,8 +112,8 @@
                                 class="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3">
                             Create
                         </button>
-                    </div>--}}
-                </div>
+                    </div>--}}{{--
+                </div>--}}
                 <div class="py-10" x-data="{newpost:false}">
                     <div class="max-w-3xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
                         <div class="hidden lg:block lg:col-span-3 xl:col-span-2">
