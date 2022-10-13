794de330516f5abec0f8d36ecbb94b60a9bb9f29
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-13 09:48:09 +0100

    Created review collection and resource
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- app/Http/Resources/ReviewCollection.php
+++ app/Http/Resources/ReviewCollection.php
@@ -14,6 +14,12 @@ class ReviewCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'links' => [
+                'documentation' => 'none',
+                'status' => 'latest'
+            ]
+        ];
     }
 }
--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -14,6 +14,11 @@ class ReviewResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'review' => $this->review,
+            'rating' => $this->rating,
+            'product' => $this->Product->name,
+            'user' => $this->User->name
+        ];
     }
 }
