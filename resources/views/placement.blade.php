1e9ee284b881495788c0c0f4744c2af959bd8e85
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-29 14:23:14 +0100

    Displaying all information now on placement.slug page
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/placement.blade.php
+++ resources/views/placement.blade.php
@@ -290,11 +290,11 @@
                                         </div>
                                         <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
                                             <dt class="text-sm font-medium text-gray-500">Role description</dt>
-                                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">(Need fresh migration)</dd>
+                                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{$placement->description}}</dd>
                                         </div>
                                         <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
                                             <dt class="text-sm font-medium text-gray-500">Requirements</dt>
-                                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">(Need fresh migration)</dd>
+                                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{$placement->requirements}}</dd>
                                         </div>
                                         <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
                                             <dt class="text-sm font-medium text-gray-500">Closing</dt>
