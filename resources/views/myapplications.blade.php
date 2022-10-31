9311e68fc294bc18f07307441329f8e585dff9ef
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-31 13:55:52 +0000

    Removed placeholder and added timestamp diff for humans instead
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/myapplications.blade.php
+++ resources/views/myapplications.blade.php
@@ -113,7 +113,7 @@
                                         <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Placement</th>
                                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Company</th>
                                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
-                                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
+                                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Applied</th>
                                         <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
                                             <span class="sr-only">Manage</span>
                                         </th>
@@ -137,7 +137,7 @@
                                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                                 <span class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">{{\Illuminate\Support\Str::title($a->status)}}</span>
                                             </td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$a->created_at->diffForHumans()}}</td>
                                             <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                                                 <a href="{{route('application.id', $a->id)}}" class="text-indigo-600 hover:text-indigo-900">Manage</a>
                                             </td>
