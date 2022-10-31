5ec9d65b13d2bf5a031bc95fadf93c7fae2300c6
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-31 13:33:35 +0000

    Removed unnessasary add button on my applications page
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/myapplications.blade.php
+++ resources/views/myapplications.blade.php
@@ -102,9 +102,6 @@
                         <h1 class="text-xl font-semibold text-gray-900">My placement applications</h1>
                         <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
                     </div>
-                    <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                        <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add user</button>
-                    </div>
                 </div>
                 <div class="mt-8 flex flex-col">
                     <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
