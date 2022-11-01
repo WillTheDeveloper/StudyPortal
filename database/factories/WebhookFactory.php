88bf6dc8d44a22251277d88616b9109573391d32
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-11-01 08:43:39 +0000

    Constructed the webhook factory
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- database/factories/WebhookFactory.php
+++ database/factories/WebhookFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use App\Models\Webhook;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -15,7 +16,14 @@ class WebhookFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::query()->get('id')->random(),
+            'url' => $this->faker->url,
+            'name' => $this->faker->name,
+            'posts' => $this->faker->boolean('50'),
+            'comments' => $this->faker->boolean('50'),
+            'assignments' => $this->faker->boolean('50'),
+            'blog' => $this->faker->boolean('50'),
+            'active' => $this->faker->boolean('50'),
         ];
     }
 }
