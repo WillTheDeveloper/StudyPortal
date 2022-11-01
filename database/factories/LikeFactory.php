4f2c78e65712da30d4ce6cb2f6f317d5f54879b8
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-11-01 11:34:42 +0000

    Created a like model factory
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Like;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Like>
+ */
+class LikeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Like::class;
+
+    public function definition()
+    {
+        return [
+            'post_id' => Post::query()->get('id')->random(),
+            'user_id' => User::query()->get('id')->random()
+        ];
+    }
+}
