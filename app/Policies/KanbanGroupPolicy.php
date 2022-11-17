efa770ad1b4ce883475ba71d9b013c348991eea3
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-11-17 15:23:14 -0500

    Completed the KanbanGroupPolicy.php file
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- app/Policies/KanbanGroupPolicy.php
+++ app/Policies/KanbanGroupPolicy.php
@@ -18,7 +18,7 @@ class KanbanGroupPolicy
      */
     public function viewAny(User $user)
     {
-        //
+        return auth()->check() && !auth()->user()->is_banned;
     }
 
     /**
@@ -30,7 +30,7 @@ class KanbanGroupPolicy
      */
     public function view(User $user, KanbanGroup $kanbanGroup)
     {
-        //
+        return auth()->check() && auth()->user()->id == $kanbanGroup->user_id;
     }
 
     /**
@@ -41,7 +41,7 @@ class KanbanGroupPolicy
      */
     public function create(User $user)
     {
-        //
+        return auth()->check() && !auth()->user()->is_banned;
     }
 
     /**
@@ -53,7 +53,7 @@ class KanbanGroupPolicy
      */
     public function update(User $user, KanbanGroup $kanbanGroup)
     {
-        //
+        return auth()->check() && !auth()->user()->is_banned && auth()->id() == $kanbanGroup->user_id;
     }
 
     /**
@@ -65,7 +65,7 @@ class KanbanGroupPolicy
      */
     public function delete(User $user, KanbanGroup $kanbanGroup)
     {
-        //
+        return auth()->check() && !auth()->user()->is_banned && auth()->id() == $kanbanGroup->user_id;
     }
 
     /**
