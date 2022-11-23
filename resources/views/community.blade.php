b5710ec1c805ec0791a7011f6ff8eba63bad83e7
Will Burton <willburton@linney.com>
2022-11-23 09:18:05 +0000

    Removed filters at the top of the main community page
    Signed-off-by: Will Burton <willburton@linney.com>

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -279,47 +279,7 @@
                                 </div>
                             </div>
 
-
-                            <div class="px-4 sm:px-0">
-                                <div class="sm:hidden">
-                                    <label for="question-tabs" class="sr-only">Select a tab</label>
-                                    <select id="question-tabs"
-                                            class="block w-full rounded-md border-gray-300 text-base font-medium text-gray-900 shadow-sm focus:border-rose-500 focus:ring-rose-500">
-                                        <option selected>Latest</option>
-
-                                        <option>Highest comments</option>
-
-                                        <option>Most Views</option>
-                                    </select>
-                                </div>
-                                <div class="hidden sm:block">
-                                    <nav class="relative z-0 rounded-lg shadow flex divide-x divide-gray-200"
-                                         aria-label="Tabs">
-                                        <!-- Current: "text-gray-900", Default: "text-gray-500 hover:text-gray-700" -->
-                                        <a href="#" aria-current="page"
-                                           class="text-gray-900 rounded-l-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
-                                            <span>Latest</span>
-                                            <span aria-hidden="true"
-                                                  class="bg-rose-500 absolute inset-x-0 bottom-0 h-0.5"></span>
-                                        </a>
-
-                                        <a href="#"
-                                           class="text-gray-500 hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
-                                            <span>Highest comments</span>
-                                            <span aria-hidden="true"
-                                                  class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
-                                        </a>
-
-                                        <a href="#"
-                                           class="text-gray-500 hover:text-gray-700 rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
-                                            <span>Most Views</span>
-                                            <span aria-hidden="true"
-                                                  class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
-                                        </a>
-                                    </nav>
-                                </div>
-                            </div>
-                            <div class="mt-4">
+                            <div>
                                 <h1 class="sr-only">Recent questions</h1>
                                 <ul role="list" class="space-y-4">
                                     @forelse($posts as $post)
