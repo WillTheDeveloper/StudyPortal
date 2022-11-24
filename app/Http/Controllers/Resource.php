5b7c9fdf98baf2a7ec5cebceb2824966eda1213a
Will Burton <willburton@linney.com>
2022-11-24 07:22:51 +0000

    Created some placeholder resource controller endpoints
    Signed-off-by: Will Burton <willburton@linney.com>

--- app/Http/Controllers/Resource.php
+++ app/Http/Controllers/Resource.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class Resource extends Controller
 {
-    //
+    public function main()
+    {
+
+    }
+
+    public function search()
+    {
+
+    }
+
+    public function results()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
 }
