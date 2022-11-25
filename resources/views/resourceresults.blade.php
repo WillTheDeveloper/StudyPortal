1a18d2e6fb22ad6782071d7d952e4de4aafdc3c6
Will Burton <willburton@linney.com>
2022-11-25 11:01:26 +0000

    Completed the search search results and added star button because awesome
    Signed-off-by: Will Burton <willburton@linney.com>

--- resources/views/resourceresults.blade.php
+++ resources/views/resourceresults.blade.php
@@ -136,6 +136,38 @@
                 </div>
 
 
+                <div class="px-8 pt-6">
+                    <div class="overflow-hidden bg-white shadow sm:rounded-md">
+                        <ul role="list" class="divide-y divide-gray-200">
+                            @foreach($results as $r)
+                                <li>
+                                    <a href="{{route('resource.id', $r->id)}}" class="block hover:bg-gray-50">
+                                        <div class="px-4 py-4 sm:px-6">
+                                            <div class="flex items-center justify-between">
+                                                <p class="truncate text-sm font-medium text-indigo-600">{{$r->title}}</p>
+                                                    <div class="ml-2 flex flex-shrink-0">
+                                                        <button type="button" class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Star</button>
+                                                    </div>
+                                            </div>
+                                            <div class="mt-2 sm:flex sm:justify-between">
+                                                <div class="sm:flex">
+                                                    <p class="flex items-center text-sm text-gray-500">
+                                                        <!-- Heroicon name: mini/users -->
+                                                        <svg class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                            <path d="M7 8a3 3 0 100-6 3 3 0 000 6zM14.5 9a2.5 2.5 0 100-5 2.5 2.5 0 000 5zM1.615 16.428a1.224 1.224 0 01-.569-1.175 6.002 6.002 0 0111.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 017 18a9.953 9.953 0 01-5.385-1.572zM14.5 16h-.106c.07-.297.088-.611.048-.933a7.47 7.47 0 00-1.588-3.755 4.502 4.502 0 015.874 2.636.818.818 0 01-.36.98A7.465 7.465 0 0114.5 16z" />
+                                                        </svg>
+                                                        {{$r->Subject->subject}}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+
+                </div>
 
 
             </main>
