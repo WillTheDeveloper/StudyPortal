d42a39f5c6aa842a01f71831900dcefef952068d
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-11-27 22:07:03 +0000

    Updated label for dropdown
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/newresource.blade.php
+++ resources/views/newresource.blade.php
@@ -217,7 +217,7 @@
 
                                                 <div class="col-span-6 sm:col-span-3">
                                                     <label for="country"
-                                                           class="block text-sm font-medium text-gray-700">Private</label>
+                                                           class="block text-sm font-medium text-gray-700">Private or public</label>
                                                     <select id="country" name="country" autocomplete="country-name"
                                                             class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
                                                         <option selected value="1">Private</option>
