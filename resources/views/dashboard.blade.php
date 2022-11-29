0063608ff9ac9f5856c0eb9fc103ee9ffe7ef002
Will Burton <willburton@linney.com>
2022-11-29 12:32:45 +0000

    Hidden recent activity until it becomes populated
    Signed-off-by: Will Burton <willburton@linney.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -243,6 +243,7 @@
 
                     </div>
 
+                    @if($notifications->count() > 0)
                     <h2 class="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">Recent activity</h2>
 
                     <!-- Activity list (smallest breakpoint only) -->
@@ -351,6 +352,7 @@
                             </div>
                         </div>
                     </div>
+                    @endif
                 </div>
             </main>
         </div>
