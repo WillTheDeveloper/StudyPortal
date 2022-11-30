ff3eccbdae9ee86afb58be97f23ef2f9be2ca2bf
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-11-30 20:54:24 +0000

    Added link to documentation on api keys page
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/apikeys.blade.php
+++ resources/views/apikeys.blade.php
@@ -102,6 +102,9 @@
                             <p class="mt-2 text-sm text-gray-700">A list of all your API keys and their abilities.</p>
                         </div>
                         <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                            <a href="{{route('docs.v1.root')}}">
+                                <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-400 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">API Documentation</button>
+                            </a>
                             <a href="{{route('keys.new')}}">
                                 <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Generate new key</button>
                             </a>
