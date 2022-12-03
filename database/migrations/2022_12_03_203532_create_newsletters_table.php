d69bcedab4ba87e9d5af8d7dec50681702d95f10
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-12-03 20:57:34 +0000

    Created newsletter stuff in backend
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

---
+++ database/migrations/2022_12_03_203532_create_newsletters_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('newsletters', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('email');
+            $table->dateTime('email_verified_at')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('newsletters');
+    }
+};
