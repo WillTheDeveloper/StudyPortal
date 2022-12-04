c249bc7160ff61a01753785bb66dede297f07238
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-12-04 11:12:23 +0000

    Changed button to anchor on managing subjects to create subjects
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/managesubjects.blade.php
+++ resources/views/managesubjects.blade.php
@@ -103,7 +103,7 @@
 {{--                            <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>--}}
                         </div>
                         <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                            <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">New subject</button>
+                            <a href="#" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">New subject</a>
                         </div>
                     </div>
                     <div class="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
