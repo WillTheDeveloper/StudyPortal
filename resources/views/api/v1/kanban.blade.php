10a1812a4c15ac9741b874875e465317086246bf
Will Burton <willburton@linney.com>
2022-12-05 11:44:30 +0000

    Kanban api docs wip
    Signed-off-by: Will Burton <willburton@linney.com>

--- resources/views/api/v1/kanban.blade.php
+++ resources/views/api/v1/kanban.blade.php
@@ -175,13 +175,67 @@
                                         <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                                             <div class="flex items-center">
                                                 <div>
-                                                    <div class="font-medium text-gray-900">Title</div>
+                                                    <div class="font-medium text-gray-900">Name</div>
                                                     {{--<div class="text-gray-500">lindsay.walton@example.com</div>--}}
                                                 </div>
                                             </div>
                                         </td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                             <div class="text-gray-900">String</div>
+                                            {{--<div class="text-gray-500">Optimization</div>--}}
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            <span class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">Yes</span>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                                            <div class="flex items-center">
+                                                <div>
+                                                    <div class="font-medium text-gray-900">Description</div>
+                                                    {{--<div class="text-gray-500">lindsay.walton@example.com</div>--}}
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            <div class="text-gray-900">String</div>
+                                            {{--<div class="text-gray-500">Optimization</div>--}}
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            <span class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">Yes</span>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                                            <div class="flex items-center">
+                                                <div>
+                                                    <div class="font-medium text-gray-900">User</div>
+                                                    {{--<div class="text-gray-500">lindsay.walton@example.com</div>--}}
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            <div class="text-gray-900">String</div>
+                                            {{--<div class="text-gray-500">Optimization</div>--}}
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            <span class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">Yes</span>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                                            <div class="flex items-center">
+                                                <div>
+                                                    <div class="font-medium text-gray-900">Created</div>
+                                                    {{--<div class="text-gray-500">lindsay.walton@example.com</div>--}}
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            <div class="text-gray-900">Timestamp</div>
                                             {{--<div class="text-gray-500">Optimization</div>--}}
                                         </td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
