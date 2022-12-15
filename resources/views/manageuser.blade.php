d0dbc12e3500a8e1763141a73c279da5c9954646
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-12-15 07:31:47 +0000

    Fixed institution showing raw data on managing users page
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/manageuser.blade.php
+++ resources/views/manageuser.blade.php
@@ -144,7 +144,7 @@
                                                 </div>
                                                 <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
                                                     <dt class="text-sm font-medium text-gray-500">Institution</dt>
-                                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{$user->Institution}}</dd>
+                                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{$user->Institution->institution}}</dd>
                                                 </div>
                                                 <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
                                                     <dt class="text-sm font-medium text-gray-500">Account created</dt>
