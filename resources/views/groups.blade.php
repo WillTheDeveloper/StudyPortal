9f75f963fcf6380a92caf5d848c7138582810785
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-12-23 21:36:26 +0000

    Removed redundant ui from groups page
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/groups.blade.php
+++ resources/views/groups.blade.php
@@ -105,16 +105,6 @@
                             Dashboard
                         </h1>
                     </div>
-                    <div class="mt-4 flex sm:mt-0 sm:ml-4">
-                        <button type="button"
-                                class="order-1 ml-3 inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-0 sm:ml-0">
-                            Share
-                        </button>
-                        <button type="button"
-                                class="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3">
-                            Create
-                        </button>
-                    </div>
                 </div>
                 <div class="p-6 bg-white border-b border-gray-200">
                     Manage groups
