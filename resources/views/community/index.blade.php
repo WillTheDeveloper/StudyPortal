61b45430692849334761daecccbee7cb37edbd42
WillTheDeveloper <willthedeveloper13@gmail.com>
2023-01-19 15:00:12 +0000

    Updated all communities pages to use SP logo instead of search
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -76,23 +76,10 @@
                 </button>
                 <div class="flex-1 flex justify-between px-4 sm:px-6 lg:px-8">
                     <div class="flex-1 flex">
-                        <form class="w-full flex md:ml-0" action="{{ route('community.search') }}" method="GET">
-                            <label for="search" class="sr-only">Search</label>
-                            <div class="relative w-full text-gray-400 focus-within:text-gray-600">
-                                <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                                    <!-- Heroicon name: solid/search -->
-                                    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                        fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd"
-                                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                                <input id="search" name="search"
-                                    class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-0 focus:border-transparent focus:placeholder-gray-400 sm:text-sm"
-                                    placeholder="Search" type="search">
-                            </div>
-                        </form>
+                        <div class="flex-shrink-0 flex items-center">
+                            <img class="block lg:hidden h-8 w-auto" src="https://studyportal.s3.eu-west-2.amazonaws.com/logo_banner_style_v1.png" alt="Workflow">
+                            <img class="hidden lg:block h-8 w-auto" src="https://studyportal.s3.eu-west-2.amazonaws.com/logo_banner_style_v1.png" alt="Workflow">
+                        </div>
                     </div>
                     <div class="flex items-center">
                         <!-- Profile dropdown -->
