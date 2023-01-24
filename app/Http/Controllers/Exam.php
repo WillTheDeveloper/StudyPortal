c8c01e397537294577188f743111836f7b4eee05
Will Burton <willburton@linney.com>
2023-01-24 12:14:26 +0000

    Added queries for exam controller on index endpoint
    Signed-off-by: Will Burton <willburton@linney.com>

--- app/Http/Controllers/Exam.php
+++ app/Http/Controllers/Exam.php
@@ -10,7 +10,9 @@ class Exam extends Controller
     public function index()
     {
         return view('exam.exams', [
-            'upcoming' => \App\Models\Exam::query()->where('tutor_id', auth()->id())->whereDate('examdate', '>', Carbon::today()->toDate())->get()
+            'upcoming' => \App\Models\Exam::query()->where('tutor_id', auth()->id())->whereDate('examdate', '>', Carbon::today()->toDate())->get(),
+            'current' => \App\Models\Exam::query()->where('tutor_id', auth()->id())->whereDate('examdate' , '=', Carbon::today()->toDate())->get(),
+            'past' => \App\Models\Exam::query()->where('tutor_id', auth()->id())->whereDate('examdate', '<', Carbon::today()->toDate())->get()
         ]);
     }
 }
