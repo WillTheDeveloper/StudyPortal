c2a03b11e0e0d59301b735db977105eef2e77601
WillTheDeveloper <willthedeveloper13@gmail.com>
2023-04-19 16:33:49 +0100

    Updated all the community pages to use the new layout
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/community/post.blade.php
+++ resources/views/community/post.blade.php
@@ -1,9 +1,4 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Community') }}
-        </h2>
-    </x-slot>
+<x-new-user-layout>
 
     <!-- Main column -->
     <div class="lg:pl-64 flex flex-col" x-data="{likes:false, commented:true}">
@@ -131,80 +126,6 @@
 
                                     <!-- More people... -->
                                 </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <!-- Search header -->
-        <div class="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white border-b border-gray-200 lg:hidden">
-            <!-- Sidebar toggle, controls the 'sidebarOpen' sidebar state. -->
-            <button @click="menu = true" type="button"
-                    class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500 lg:hidden">
-                <span class="sr-only">Open sidebar</span>
-                <!-- Heroicon name: outline/menu-alt-1 -->
-                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M4 6h16M4 12h8m-8 6h16"/>
-                </svg>
-            </button>
-            <div class="flex-1 flex justify-between px-4 sm:px-6 lg:px-8">
-                <div class="flex-1 flex">
-                    <div class="flex-shrink-0 flex items-center">
-                        <img class="block lg:hidden h-8 w-auto" src="https://studyportal.s3.eu-west-2.amazonaws.com/logo_banner_style_v1.png" alt="Workflow">
-                        <img class="hidden lg:block h-8 w-auto" src="https://studyportal.s3.eu-west-2.amazonaws.com/logo_banner_style_v1.png" alt="Workflow">
-                    </div>
-                </div>
-                <div class="flex items-center">
-                    <!-- Profile dropdown -->
-                    <div class="ml-3 relative">
-                        <div>
-                            <button @click="profile = true" type="button"
-                                    class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
-                                    id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full"
-                                     src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                     alt="">
-                            </button>
-                        </div>
-
-                        <!--
-                          Dropdown menu, show/hide based on menu state.
-
-                          Entering: "transition ease-out duration-100"
-                            From: "transform opacity-0 scale-95"
-                            To: "transform opacity-100 scale-100"
-                          Leaving: "transition ease-in duration-75"
-                            From: "transform opacity-100 scale-100"
-                            To: "transform opacity-0 scale-95"
-                        -->
-                        <div x-show="profile" x-cloak
-                             class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 focus:outline-none"
-                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
-                             tabindex="-1">
-                            <div class="py-1" role="none">
-                                <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                                <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem"
-                                   tabindex="-1" id="user-menu-item-0">View profile</a>
-                                <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem"
-                                   tabindex="-1" id="user-menu-item-1">Settings</a>
-                                <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem"
-                                   tabindex="-1" id="user-menu-item-2">Notifications</a>
-                            </div>
-                            <div class="py-1" role="none">
-                                <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem"
-                                   tabindex="-1" id="user-menu-item-3">Get desktop app</a>
-                                <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem"
-                                   tabindex="-1" id="user-menu-item-4">Support</a>
-                            </div>
-                            <div class="py-1" role="none">
-                                <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem"
-                                   tabindex="-1" id="user-menu-item-5">Logout</a>
                             </div>
                         </div>
                     </div>
@@ -430,4 +351,4 @@
         </div>
     </div>
     </div>
-</x-app-layout>
+</x-new-user-layout>
